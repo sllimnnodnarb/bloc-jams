9447c266fc1cb799bf38fff4a77c732299baa281
Brandon Mills <sllimnodnarb@gmail.com>
2017-01-16 13:53:36 -0600

    adding all files associated with assignment11

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumMarconi = {
      ]
  };
 
+var albumCounts = {
+     title: 'Numbers',
+     artist: 'Sir Counts-A-Lot',
+     label: 'EM',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/count.jpg',
+     songs: [
+         { title: 'One', duration: '0:01' },
+         { title: 'Two', duration: '0:02' },
+         { title: 'Three', duration: '0:03'},
+         { title: 'Four', duration: '0:04' },
+         { title: 'Five', duration: '0:05'}
+     ]
+ };
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -40,24 +55,22 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+
  var setCurrentAlbum = function(album) {
-    
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-    
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
- 
-     
+    
      albumSongList.innerHTML = '';
  
-     
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -65,4 +78,16 @@ var createSongRow = function(songNumber, songName, songLength) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
- };
+ 
+    var albums =[albumPicasso, albumMarconi, albumCounts];
+    var index = 1;
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if(index == albumArray.length) {
+            index = albumArray[0];
+        }
+    });
+};
+   
+   
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,4 @@
- var pointsArray = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
  
 
 var revealPoint = function (point) {
